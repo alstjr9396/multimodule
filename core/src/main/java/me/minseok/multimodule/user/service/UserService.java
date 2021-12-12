46d60613f1385724668baa6c30a80ee16a4ac2ed
kang <minseokkang95@gmail.com>
2021-12-12 14:07:16 +0900

    feat : 유저 도메인 생성

---
+++ core/src/main/java/me/minseok/multimodule/user/service/UserService.java
@@ -0,0 +1,10 @@
+package me.minseok.multimodule.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+}
